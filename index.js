e9e7273e2353516e746ea4107837d6b77f102e7c
jonathonpday <jonathonpday@me.com>
2024-04-25 09:57:59 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// Compound interest calculator. Will prompt a user for inputs and calculate compound interest value.
+
+let init_amount = 20000;
+let monthly_contribution = 400;
+let number_of_years = 30;
+let interest_rate = 10;
+
+// 1. Define a function that we can use to calculate the final value of compound interest.
+
+// 2. Define a function that would calculate the difference (effect compounding has).
+
+// 3. To create a run function to prompt the user for all necessary inputs required to calculate final amounts.
+
+// 4. Make a print statement using template literal.
